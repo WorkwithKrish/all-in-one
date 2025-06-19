09c5bd5f6306a0f1ceb1958c8bc256492d36b135
Krishna <manifester2@outlook.com>
2025-06-19 17:19:46 +0530

    Feat: Create Chip Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import ChipInputDemo from "./features/chipInput/ChipInputDemo";
 import Todo from "./features/todos/Todo";
 
 export default function App() {
   return (
     <div className="App">
       <Todo />
+      <ChipInputDemo />
     </div>
   );
 }
